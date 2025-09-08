466be8c3007bd0055be0fbe6b8603ca3d4e89637
Narayanipriyanka <gundapanthulaarunakumari@gmail.com>
2025-09-08 09:40:49 +0530

    made enhancements

--- taskmanagement/src/main/java/com/taskhub/taskmanagement/config/DatabaseConfig.java
+++ taskmanagement/src/main/java/com/taskhub/taskmanagement/config/DatabaseConfig.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -14,6 +15,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "com.taskhub.taskmanagement.repository")
 public class DatabaseConfig {
     @Bean
     public DataSource dataSource() {
