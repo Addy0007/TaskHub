e6075e7d5b668626fb1dbc93d5e661ee5b19076f
Narayanipriyanka <gundapanthulaarunakumari@gmail.com>
2025-09-10 10:36:17 +0530

    made enhancements

--- taskmanagement/src/main/java/com/taskhub/taskmanagement/exception/GlobalExceptionHandler.java
+++ taskmanagement/src/main/java/com/taskhub/taskmanagement/exception/GlobalExceptionHandler.java
@@ -5,15 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Object handleValidationExceptions(MethodArgumentNotValidException ex, Model model) {
@@ -33,18 +33,34 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
+    public Object handleNoSuchElementException(NoSuchElementException ex,Model model) {
+        if (model != null) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return new ModelAndView("error-page");
+        }
+        else {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }}
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public Object handleRuntimeException(RuntimeException ex,Model model) {
+        if (model != null) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return new ModelAndView("error-page");
+        }
+        else {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public Object handleNullPointerException(NullPointerException ex,Model model) {
+        if (model != null) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return new ModelAndView("error-page");
+        } else {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @ExceptionHandler(Exception.class)
